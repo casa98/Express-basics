333de6e0365bfed8fa175d8514e1378e661ec488
andressaggudelo <andresagudelo9239@gmail.com>
2020-06-18 00:56:43 -0500

    Add some static files

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/user', (req, res)=>{
 app.post('/user/:id', (req, res)=>{
     console.log(req.body);
     console.log(req.params);
-    res.send('POST REQUEST RECEIVED');
+    res.send('POST REQUEST RECEIVED'); 
 });
 
 app.put('/user/:id', (req, res)=>{
@@ -29,6 +29,11 @@ app.put('/user/:id', (req, res)=>{
 app.delete('/user/:id', (req, res)=>{
     res.send("User "+req.params.id+" was deleted");
 })
+
+// IMPORTANT, to serve static files to front-end:
+// Create 'public' dir in project root
+app.use(express.static('public'));
+
 
 app.listen(3000, ()=>{
     console.log("Server running on port 3000");
